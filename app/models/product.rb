83e28591db4a04ede3b5bb0ee7cc3d4bd4f98a80
stasfly <s19f84@gmail.com>
2022-10-11 14:38:13 +0300

    Add rubocop, Pry

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   validates :name, length: { maximum: 100 }
   validates :description, length: { in: 3..1200,
-                                    too_long: "%{count} characters is max allowed",
-                                    too_short: "%{count} characters is min allowed"},
-                                    allow_nil: true
-  validates :product_type, length: { maximum: 20, too_long: "%{count} characters is max allowed" }
+                                    too_long: '%<count>s characters is max allowed',
+                                    too_short: '%<count>s characters is min allowed' },
+                          allow_nil: true
+  validates :product_type, length: { maximum: 20, too_long: '%<count>s characters is max allowed' }
   validates :product_code, numericality: { only_integer: true,
-                                          message: -> (object, data) do
-                                            if object.product_code.nil? 
-                                              "Please insert unique product code"
-                                            elsif object.product_code == data[:value]
-                                              "Another object has already contained: #{data[:value]} value"
-                                            else
-                                              "#{data[:value]} seems to be not integer"
-                                            end
-                                          end },
-                          uniqueness: true, on: :update
-
+                                           message: lambda do |object, data|
+                                                      if object.product_code.nil?
+                                                        'Please insert unique product code'
+                                                      elsif object.product_code == data[:value]
+                                                        "Another object has already contained: #{data[:value]} value"
+                                                      else
+                                                        "#{data[:value]} seems to be not integer"
+                                                      end
+                                                    end },
+                           uniqueness: true, on: :update
 end
