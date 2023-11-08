97ea5efdfa53b4561cc4bdcd79286288ac728fe8
stasfly <s19f84@gmail.com>
2023-11-08 12:57:30 +0200

    Configs for volume persistent storage on fly.io. Needs to have synchronisation. Row variant for two VM instances do not work

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,46 +16,46 @@ user.skip_confirmation_notification!
 user.skip_confirmation!
 user.save
 
-# unless Rails.env.production?
-#   require 'factory_bot_rails'
+unless Rails.env.production?
+  require 'factory_bot_rails'
 
-#   50.times do |n|
-#     user = User.new(
-#       email: Faker::Internet.email,
-#       password: 'qwerty',
-#       password_confirmation: 'qwerty')
-#     user.add_role :user
-#     user.skip_confirmation_notification!
-#     user.skip_confirmation!
-#     user.save
-#     user.cart = Cart.create
-#   end
+  50.times do |n|
+    user = User.new(
+      email: Faker::Internet.email,
+      password: 'qwerty',
+      password_confirmation: 'qwerty')
+    user.add_role :user
+    user.skip_confirmation_notification!
+    user.skip_confirmation!
+    user.save
+    user.cart = Cart.create
+  end
 
-#   5.times do |n|
-#     ProductCategory.create(name: "Cat#{n + 1}", description: Faker::Lorem.paragraph)
-#   end
+  5.times do |n|
+    ProductCategory.create(name: "Cat#{n + 1}", description: Faker::Lorem.paragraph)
+  end
 
-#   product_categories = ProductCategory.all
-#   10.times do |n|
-#     product = Product.new(
-#       name: "item_#{n}",
-#       description: Faker::Lorem.paragraph,
-#       sku: "sku_#{n}",
-#       price: rand(1000)
-#     )
-#     product.product_category_id = product_categories.sample.id
-#     product.save
-#     ProductInventory.find_by(product_id: product.id).update(quantity: rand(50))
-#   end
+  product_categories = ProductCategory.all
+  10.times do |n|
+    product = Product.new(
+      name: "item_#{n}",
+      description: Faker::Lorem.paragraph,
+      sku: "sku_#{n}",
+      price: rand(1000)
+    )
+    product.product_category_id = product_categories.sample.id
+    product.save
+    ProductInventory.find_by(product_id: product.id).update(quantity: rand(50))
+  end
 
-#   products = Product.all
-#   carts = Cart.first(10)
-#   2.times do |n|
-#     cart_item = CartItem.new(
-#       cart_id: carts.sample.id,
-#       product_id: products.sample.id,
-#       quantity: rand(5)
-#     )
-#     cart_item.save  
-#   end
-# end
+  products = Product.all
+  carts = Cart.first(10)
+  2.times do |n|
+    cart_item = CartItem.new(
+      cart_id: carts.sample.id,
+      product_id: products.sample.id,
+      quantity: rand(5)
+    )
+    cart_item.save  
+  end
+end
